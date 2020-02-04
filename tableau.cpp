96c1af761e7370ffc64de06c806160279c41a5f1
petergrajcar <peter.grajcar131@gmail.com>
2020-02-04 11:29:39 +0100

    prepare the header file

---
+++ tableau.cpp
@@ -0,0 +1,34 @@
+#include "tableau.hpp"
+#include <string>
+#include <memory>
+#include <queue>
+
+tableau::entry::entry(bool sign, const std::string &subformula, entry *parent)
+    : sign(sign), subformula(subformula), parent(parent), left(nullptr), right(nullptr), contradictory(false)
+{
+}
+
+tableau::tableau(const std::string &formula)
+{
+    root = std::make_unique<tableau::entry>(false, formula, nullptr);
+}
+
+void tableau::append(bool sign, const std::string &formula)
+{
+    //TODO:
+}
+
+void tableau::reduce()
+{
+    //TODO:
+}
+
+bool tableau::is_finished() const
+{
+    return to_reduce.empty();
+}
+
+bool tableau::is_contradictory() const
+{
+    return root && root->contradictory;
+}
--- tableau.hpp
+++ tableau.hpp
@@ -1,7 +1,41 @@
 /***
  * 
  ***/
+#include <memory>
+#include <string>
+#include <queue>
 #ifndef TABLEAU_HPP_INCLUDED
 #define TABLEAU_HPP_INCLUDED
+
+class tableau
+{
+private:
+    class entry
+    {
+    private:
+        friend class tableau;
+
+        bool sign;
+        bool contradictory;
+        std::string subformula;
+        std::unique_ptr<entry> left;
+        std::unique_ptr<entry> right;
+        entry *parent;
+
+    public:
+        entry(bool sign, const std::string &subformula, entry *parent);
+    };
+
+private:
+    std::unique_ptr<entry> root;
+    std::queue<std::unique_ptr<entry>> to_reduce;
+
+public:
+    tableau(const std::string &formula);
+    void append(bool sign, const std::string &formula);
+    void reduce();
+    bool is_finished() const;
+    bool is_contradictory() const;
+};
 
 #endif
